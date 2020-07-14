3f521a6fd53795690c6459f02f2c2e9c33f2faac
Lionel Surmont <lionel.surmont@gmail.com>
2020-07-14 19:19:39 +0200

    changing user migration

--- db/migrate/20200711131034_add_category_and_username_to_users.rb
+++ db/migrate/20200711131034_add_category_and_username_to_users.rb
@@ -1,6 +1,6 @@
 class AddCategoryAndUsernameToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :category, :string
+    add_column :users, :category, :string, default: "owner"
     add_column :users, :username, :string
   end
 end
