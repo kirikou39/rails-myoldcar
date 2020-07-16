0afa28ff627b2ee647cbe4722a078a707f612ce7
ancastan <ankastan@ymail.com>
2020-07-16 21:39:08 +0200

    Bookings controller + views

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,38 @@
 class BookingsController < ApplicationController
+
+  before_action :set_booking, only: [:new, :create, :show]
+
+  def index
+    @bookings = Booking.all
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    @booking.amount = (@booking.end_date - @booking.start_date).to_i * @car.price
+    @booking.car = @car
+      if @booking.save
+        redirect_to booking_path(@booking)
+      else
+        render 'new'
+      end
+  end
+
+  def show
+    @booking = @car.booking
+  end
+
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :amount)
+  end
+  
+  def set_booking
+  	@car = Car.find(params[:car_id])
+  end
 end
