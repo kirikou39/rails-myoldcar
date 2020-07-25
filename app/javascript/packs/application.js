984ecfc28d8b8638231469b5eb90204668347453
Lionel Surmont <lionel.surmont@gmail.com>
2020-07-25 15:48:42 +0200

    require vs import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,7 +26,7 @@ require("channels")
 // External imports
 import "bootstrap";
 import flatpickr from "flatpickr";
-import "flatpickr/dist/flatpickr.css";
+require("flatpickr/dist/flatpickr.css");
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
