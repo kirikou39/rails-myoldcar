3802fd59b887ae29c38b57ae969abb20cf766b37
Lionel Surmont <lionel.surmont@gmail.com>
2020-07-25 14:54:47 +0200

    improve init mapbox

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -2,13 +2,13 @@ import mapboxgl from 'mapbox-gl';
 
 const initMapbox = () => {
   const mapElement = document.getElementById('map');
-  const markers = JSON.parse(mapElement.dataset.markers);
-  const fitMapToMarkers = (map, markers) => {
-    const bounds = new mapboxgl.LngLatBounds();
-    markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-    map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
-  };
   if (mapElement) { // only build a map if there's a div#map to inject into
+    const markers = JSON.parse(mapElement.dataset.markers);
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    };
     mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
     const map = new mapboxgl.Map({
       container: 'map',
